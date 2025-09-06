f604082dc33f223d18426da3649d437200d845d0
yugorpaul <nhantumboyugor@gmail.com>
2025-09-06 16:59:47 +0200

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Yugor Paul",
+    author_email="krismiidtv@gmailcom",
+    packages=find_packages(),
+    install_requires=[]
+)
